8e8bcca6b8d490f278574fb8c9f5027ad242b5fc
Florin Ionescu <florin.ionescu@everymatrix.com>
2020-10-13 17:06:45 +0300

    [fix-frontend]_remove_useless_character

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -37,7 +37,7 @@ const ProductScreen = ({ match }) => {
               />
             </ListGroup.Item>
             <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-            <ListGroup.Item>Description: ${product.description}</ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
           </ListGroup>
         </Col>
         <Col md={3}>
