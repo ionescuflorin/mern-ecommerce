910bc215337f1334ee6cb257d948dab23598f77b
Florin Ionescu <florin.ionescu@everymatrix.com>
2020-10-13 11:48:09 +0300

    [feat-backend]_fetching_products_from_db

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
-import products from './data/products.js';
 import connectDB from './config/db.js';
+
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -13,18 +14,15 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-app.get('/api/products/:id', (req, res) => {
-  // req.params.id takes ':id' from url
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+
+// mounting other routes
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
