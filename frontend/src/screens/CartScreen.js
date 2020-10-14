f6cd5395d50430254a4a7c1517bc9ae8d67ce2f2
Florin Ionescu <florin.ionescu@everymatrix.com>
2020-10-14 17:00:12 +0300

    [feat-frontend]_add_to_cart_functionality

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from 'react-bootstrap';
+import Message from '../components/Message';
+import { addToCart } from '../redux/actions/cartActions';
 
-const CartScreen = () => {
-    return (
-        <div>
-            Cart
-        </div>
-    )
-}
+const CartScreen = ({ match, location, history }) => {
+  // match - to take the id from url
+  const productId = match.params.id;
 
-export default CartScreen
+  // location - get access to query params - ?qty=1
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+
+  const dispatch = useDispatch();
+
+  const cart = useSelector(state => state.cart)
+  const {cartItems} = cart 
+
+  console.log(cartItems)
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
+  return <div>Cart</div>;
+};
+
+export default CartScreen;
