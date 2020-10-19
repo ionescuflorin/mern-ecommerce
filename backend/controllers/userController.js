a6afa9c520aa4580046ee38b0c717b2e2a4343e4
Florin Ionescu <florin.ionescu@everymatrix.com>
2020-10-19 12:44:37 +0300

    [feat-backend]_generate_jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler';
+import generateToken from '../utils/generateToken.js';
 import User from '../models/userModel.js';
 
 // @desc     Auth user & get token
@@ -15,11 +16,11 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
-      res.status(401)
-      throw new Error('Invalid email or password')
+    res.status(401);
+    throw new Error('Invalid email or password');
   }
 });
 
