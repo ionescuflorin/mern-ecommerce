2f6df89294bccf7e65b638a631c1f74311f04184
Florin Ionescu <florin.ionescu@everymatrix.com>
2020-10-19 12:32:57 +0300

    [feat-backend]_user_authentication_endpoint

--- backend/server.js
+++ backend/server.js
@@ -5,21 +5,27 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
+// ENVIRONMENT CONFIG
 dotenv.config();
 
+// DB CONNECTION
 connectDB();
 
+// EXPRESS CONFIG
 const app = express();
+// allow us to use json in body
+app.use(express.json())
 
+// MOUNTING ROUTES
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
-
-// mounting other routes
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
-// middlewares
+// MIDDLEWARES
 app.use(notFound);
 app.use(errorHandler);
 
