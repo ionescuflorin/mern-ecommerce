3cbc24ef2e235fd9444f50fb4e9eba16b1a8a280
Florin Ionescu <florin.ionescu@everymatrix.com>
2020-10-19 18:14:34 +0300

    [feat-backend]_user_registration_and_password_encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,6 +31,16 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// middleware - before we save, we encrypt the password
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model('Users', userSchema);
 
 export default User;
