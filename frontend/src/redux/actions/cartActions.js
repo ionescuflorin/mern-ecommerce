3cfe5c8e81a3a894ca71ee53f1e54ca636355fbe
Florin Ionescu <florin.ionescu@everymatrix.com>
2020-10-22 09:29:31 +0300

    [feat-frontend]_shipping_screen_and_save_address

--- frontend/src/redux/actions/cartActions.js
+++ frontend/src/redux/actions/cartActions.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+} from '../constants/cartConstants';
 
 // with getState we can access the entire state tree
 export const addToCart = (id, qty) => async (dispatch, getState) => {
@@ -28,4 +32,13 @@ export const removeFromCart = (id) => (dispatch, getState) => {
   });
 
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
+
+export const saveShippingAddress = (formData) => (dispatch) => {
+  dispatch({
+    type: CART_SAVE_SHIPPING_ADDRESS,
+    payload: formData,
+  });
+
+  localStorage.setItem('shippingAddress', JSON.stringify(formData));
 };
